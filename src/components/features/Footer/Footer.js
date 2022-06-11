5e4da86e86968715b7793d06b192c35b16908a43
Mirosław Gucwa <mgucwa@gmail.com>
2022-06-11 23:45:53 +0200

    App (layout), add table form

---
+++ src/components/features/Footer/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return (
+    <footer className="mt-4 mb-2 text-center text-muted fixed-bottom">Copyright &copy; WaiterApp 2022</footer>
+  );
+};
+
+export default Footer;
---
+++ src/settings/settings.js
@@ -0,0 +1,29 @@
+export const Config = {
+  peopleAmountMini: 0,
+  peopleAmountMax: 10,
+};
+
+export const FormErrorsMessages = {
+  thisFieldRequired: 'This field is required',
+  minLength03: 'Minimum 3 characters',
+  minLength20: 'Minimum 20 characters',
+};
+
+export const StatusListStorage = [
+  {
+    id: 'free',
+    name: 'Free'
+  },
+  {
+    id: 'reserved',
+    name: 'Reserved'
+  },
+  {
+    id: 'busy',
+    name: 'Busy'
+  },
+  {
+    id: 'cleaning',
+    name: 'Cleaning'
+  },
+];
