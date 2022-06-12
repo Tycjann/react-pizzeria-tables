72c3f6bb8100df6bc2a690f8588edef2158422b8
Mirosław Gucwa <mgucwa@gmail.com>
2022-06-12 20:31:36 +0200

    Preparation to addTable

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import initialState from './initialState';
 import tablesReducer from './tablesRedux';
 
@@ -10,7 +11,12 @@ const reducer = combineReducers(subreducers);
 const store = createStore(
   reducer,
   initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose (
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__() ?
+      window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  )
 );
 
 export default store;
